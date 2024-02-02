3a11dfa1d7c9154e5582d0f4736bcfb40fb858b6
Matt <mcbehling86@gmail.com>
2024-02-01 19:53:22 -0600

    changed the message

--- check_for_unfilled_open_hours.py
+++ check_for_unfilled_open_hours.py
@@ -12,7 +12,7 @@ one_day_from_now = int(time.time()) + 64 * 3600 #offset by 5 hours for testing
 discord_id = find_open_hours_host(RH_API_KEY, CHANNEL_ID, SERVER_ID, one_day_from_now)
 
 if discord_id == None:
-    send_discord_reminder(DISCORD_BOT_TOKEN, discord_id, TEST_CHANNEL_ID, "No one is signed up to host this evening. Please address accordingly.")
+    send_discord_reminder(DISCORD_BOT_TOKEN, discord_id, TEST_CHANNEL_ID, "No one is signed up to host tomorrow. Please address accordingly.")
 
 # TODO
 # if today is Monday:
