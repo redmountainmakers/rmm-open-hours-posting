8d27c10e89756bbcb10588fc50c969748513e65d
Matt <mcbehling86@gmail.com>
2024-02-20 20:13:23 -0600

    Rolled back to R2 api after fixing the api key

--- discord_open_hours_posting.py
+++ discord_open_hours_posting.py
@@ -58,7 +58,7 @@ async def post_event(event, channel_id):
     async with aiohttp.ClientSession() as session:
         try:
             async with session.post(
-                f"https://raid-helper.dev/api/v3/servers/{SERVER_ID}/channels/{channel_id}/event",
+                f"https://raid-helper.dev/api/v2/servers/{SERVER_ID}/channels/{channel_id}/event",
                 headers={"Authorization": RH_API_KEY, "Content-Type": "application/json; charset=utf-8"},
                 json=event,
             ) as response:
