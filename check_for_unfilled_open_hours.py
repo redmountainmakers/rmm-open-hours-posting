a5f2489aba56879bf56fdb0e443f07acfe178f72
Creidhne86 <mcbehling86@gmail.com>
2024-03-07 08:23:23 -0600

    Update check_for_unfilled_open_hours.py

--- check_for_unfilled_open_hours.py
+++ check_for_unfilled_open_hours.py
@@ -9,7 +9,13 @@ SERVER_ID = os.getenv("SERVER_ID")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 TEST_CHANNEL_ID = int(os.getenv("TEST_CHANNEL_ID"))
 
+
+log_file_path = 'hosting_reminder.log'
+logging.basicConfig(level=logging.INFO, filename= log_file_path, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info(f"Starting to look 1 day ahead for open hours host")
 one_day_from_now = int(time.time()) + 23 * 3600 #offset by 23 hours for testing doing a 24 hr notice to open hours
+print(one_day_from_now)
 
 # test_time = dt_cst = datetime(2024, 3, 12, 18, 55, tzinfo=timezone(timedelta(hours=-6)))
 # print(test_time)
@@ -18,7 +24,7 @@ one_day_from_now = int(time.time()) + 23 * 3600 #offset by 23 hours for testing 
 
 discord_id = find_open_hours_host(RH_API_KEY, CHANNEL_ID, SERVER_ID, one_day_from_now)
 
-# print(discord_id)
+print(discord_id)
 
 
 if discord_id == None:
